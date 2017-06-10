aa39a1b15d801f1107b2330e10a3566dca09169e
zytyzz <gsyta1@wp.pl>
2017-06-10 08:56:15 +0100

    add hmtl & js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//script.js
+function triangleArea(){
+  var a=document.getElementById("a").value;
+  var h=document.getElementById("h").value; 
+  var triangleArea=a*h/2;
+
+  var napis=document.getElementById("wynik").innerHTML = triangleArea;
+  napis;
+  console.log("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+}
