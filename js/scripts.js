b8bac8e8adabea6b0f6de0314221b84fe9a7555d
zytyzz <gsyta1@wp.pl>
2017-07-01 10:21:42 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 //script.js
-function triangleArea(){
+function triangleArea(){ 
   var a = document.getElementById("a").value;
   var h = document.getElementById("h").value; 
+
+  if (1*a < 0||1*a == 0){ alert("Nie wykonam obliczeń bo któraś z liczb nie jest dodatnia!");}
+  if (1*h < 0||1*h == 0){ alert("Nie wykonam obliczeń bo któraś z liczb nie jest dodatnia!");}
+
   var triangleArea = a * h / 2;
 
   document.getElementById("wynik").innerHTML = triangleArea;
